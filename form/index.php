912b30aeac28491701cbb17a2e9156992ac045c3
Rubayet Hasan <39743675+Rubayet88@users.noreply.github.com>
2018-06-11 11:58:55 +0600

    Add files via upload

---
+++ form/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+
+ <html>
+
+ <body>
+ 
+  <form name ="index" method= "get" action = "login-handler.php">
+    
+    <center>	
+    
+	<h1>Login</h1>
+	
+	
+	 <label for="uname"><b>Username:</b></label>
+     <input type="text" placeholder="Enter Username" name="user" required/>
+	 
+     <br></br>
+     <label for="psw"><b>Password:</b></label>
+	 <input type="text" placeholder="Enter Password" name="psw" required/>
+
+     <br></br>
+	 <input type = "submit" value = "submit" name ="submit"/>
+	 
+	 <br></br>
+	 <b>Or</b>
+	 
+	 <br></br>
+	 <a href = "https://www.google.com">
+	 <button id="google-login" class="google"><img src="google-login.png" /></button>
+	 </a>
+	 
+	 <br></br>
+	 <a href = "https://www.facebook.com">
+	 <button id="fb-login" class="fb"><img src="fb.png" /></button>
+	 </a>
+	 
+	 </center>
+	   
+  </form>
+ 
+ </body>
+  
+ </html>
+	 
+	 
+	
+	 
---
+++ form/login-handler.php
@@ -0,0 +1,10 @@
+<?php
+
+$un = $_GET['user'];
+$pw = $_GET['psw'];
+
+echo "<a>user name:$un and password: $pw </a>";
+
+
+
+?>
